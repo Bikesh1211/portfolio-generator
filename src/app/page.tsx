9c78780285f4747ea7a8e64b74a39b014d732c63
bidwota <giribidwota@gmail.com>
2025-05-25 09:52:17 +0545

    default theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import data from "@/data/data.json";
 import React from "react";
 import data2 from "@/components/theme-2/data.json";
 const page = () => {
-  return <Theme2 data={data2} />;
+  // return <Theme2 data={data2} />;
   return <HomePage {...data} />;
 };
 
